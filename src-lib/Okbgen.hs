d635e658ad0f1f6eeb8f70ea8700d55c7bcebedc
danieljentsch <dev@danieljentsch.com>
2024-12-06 23:12:56 +0100

    Added experimental tolerant argument constructor for Okbgen.okbgen
    tolerantParamBuilder takes a standard parameter for okbgen and
    three lists and tries to make some meaningful parameter for okbgen
    out of it.

--- src-lib/Okbgen.hs
+++ src-lib/Okbgen.hs
@@ -2,11 +2,16 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
 
-module Okbgen (Color(..), OkbParam(..), okbgen, Element) where
+module Okbgen (Color(..), mkColorHTML, getColorCode, stdParams, orBlack, OkbParam(..), tolerantParamBuilder, okbgen, Element) where
 
 import Grid (Coordinates(..), ingrid2)
 import EulerGrid (EulerGrid(..), EulerGridCoord(..), PlainCoord(..), keyCorners)
+import Data.Word (Word8)
+import Data.Char (digitToInt)
+import Numeric (showHex)
+import Text.Printf (printf)
 
 import Graphics.Svg (Element,
                      doctype,
@@ -32,11 +37,48 @@ import qualified Data.Text as T (toTitle, pack)
 import qualified Data.Ix as DI (inRange)
 import qualified PlainGeometry as PG (toPair)
 
+data Color = RGB Word8 Word8 Word8 deriving (Show,Eq)
 
-data Color = HTMLColor { getColorCode :: String }
 
-labelColor = HTMLColor "#000000"
-borderColor = HTMLColor "#000000"
+black :: Color
+black = RGB 0 0 0
+
+orBlack :: Maybe Color -> Color
+orBlack (Just color) = color
+orBlack Nothing = black
+
+getColorCode :: Color -> String
+getColorCode (RGB r g b) = printf ("#" ++ concat (replicate 3 "%02x")) r g b
+
+mkColorHTML :: String -> Maybe Color
+mkColorHTML str = case str of
+  '#':rst -> let len = length rst in
+               if len == length (filter (flip (elem @[]) "0123456789abcdef") rst)
+               then
+                 case len of
+                   6 -> Just $ let ([r,g,b],_,_) =
+                                     foldr (\hexDigit (accl, digitIndex, lastDigit) -> (case digitIndex of
+                                                                                          0 -> (accl, 1, (fromIntegral $ digitToInt hexDigit))
+                                                                                          1 -> ((lastDigit+16*(fromIntegral $ digitToInt hexDigit)):accl, 0, 0)))
+                                           ([], 0, 0)
+                                           rst
+                               in
+                                 RGB r g b
+                   3 -> Just $ let [r,g,b] = foldr (\hexDigit accl -> ((16+1)*(fromIntegral $ digitToInt hexDigit)):accl) [] rst in (RGB r g b)
+                   _ -> Nothing
+                 else Nothing
+  _       -> Nothing
+
+interColor :: Color -> Color -> Float -> Color
+interColor c@(RGB cr cg cb) d@(RGB dr dg db) f
+    | f <= 0    = c
+    | f >= 1    = d
+    | otherwise = RGB (wm cr dr) (wm cg dg) (wm cb db)
+  where
+    wm a b = round $ (1-f) * (fromIntegral a) + f * (fromIntegral b)
+
+labelColor = RGB 0 0 0
+borderColor = RGB 0 0 0
 
 -- data Layout = LayoutNode
 
@@ -73,12 +115,132 @@ tonename octaves quints gterzes = commata <> oktavetonename
     qrflat                                             = ["fes","ces","ges","des","as","es","b"] :: [Text]
 
 data OkbParam = OkbParam { opCanvasSize :: (Float, Float),
-                           opOriginOnCanvas :: (Float, Float),
                            opCanvasPxPerUnit :: (Float, Float),
+                           opOriginOnCanvas :: (Float, Float),
                            opGridOctavesRange :: (Int, Int),
                            opGridFifthsRange :: (Int, Int),
                            opGridThirdsRange :: (Int, Int),
                            opKeyColors :: (Int -> Color) }
+
+-- standard values
+stdColors :: Int -> String
+stdColors (-2) = "#0f0e14"
+stdColors (-1) = "#9db2c5"
+stdColors    0 = "#c2c0b9"
+stdColors    1 = "#c48569"
+stdColors    2 = "#3f2a1e"
+stdColors    _ = "#ffffff"
+
+stdHeight, stdWidth, stdScalex, stdScaley, stdOriginx, stdOriginy :: Float
+stdHeight = 1080
+stdWidth = 1920
+stdScalex = 400
+stdScaley = -400
+stdOriginx = 1000
+stdOriginy = 540
+
+stdOctsMin, stdOctsMax, stdFifthsMin, stdFifthsMax, stdThirdsMin, stdThirdsMax :: Int
+stdOctsMin = -8
+stdOctsMax = 8
+stdFifthsMin = -10
+stdFifthsMax = 10
+stdThirdsMin = -2
+stdThirdsMax = 2
+
+stdParams :: OkbParam
+stdParams =  OkbParam { opCanvasSize = (stdWidth, stdHeight),
+                        opCanvasPxPerUnit = (stdScalex, stdScaley),
+                        opOriginOnCanvas = (stdOriginx, stdOriginy),
+                        opGridOctavesRange = (stdOctsMin, stdOctsMax),
+                        opGridFifthsRange = (stdFifthsMin, stdFifthsMax),
+                        opGridThirdsRange = (stdThirdsMin, stdThirdsMax),
+                        opKeyColors = orBlack . mkColorHTML . stdColors }
+
+
+-- may throw an error!  check arguments
+rawParamBuilder :: [Float] -> [Int] -> [Color] -> OkbParam
+rawParamBuilder [c1,c2,c3,c4,c5,c6] [g1,g2,g3,g4,g5,g6] colors =
+  OkbParam (c1, c2) (c3, c4) (c5, c6) (g1, g2) (g3, g4) (g5, g6) colFun
+  where
+    colFun x | x >= g5 && x <= g6 = colors!!(x - g5)
+             | otherwise          = colors!!(g6 - g5 + 1)
+
+rawParamDeconstructor :: OkbParam -> ([Float],[Int],[Color])
+rawParamDeconstructor (OkbParam (c1, c2) (c3, c4) (c5, c6) (g1, g2) (g3, g4) (g5, g6) colFun) =
+  ([c1,c2,c3,c4,c5,c6], [g1,g2,g3,g4,g5,g6], colors)
+  where
+    colors = map colFun [g5..(g6+1)]
+
+--TODO: Check if (a == 0) false implies that (x / a) is not an error (for floats).
+updateHeightKeepRatio oldWidth oldHeight newWidth
+  | oldWidth == 0  = oldHeight
+  | otherwise      = oldHeight * newWidth / oldWidth
+
+-- try to center an integer range around center of given range; if necessary, move slightly up
+updateRange :: Int -> Int -> Int -> (Int, Int)
+updateRange oldMin oldMax newDiff =
+  let twiceUpper = oldMin + oldMax + newDiff
+      twiceLower = oldMin + oldMax - newDiff in
+  if even twiceUpper
+  then (twiceUpper `div` 2, twiceLower `div` 2)
+  else ((twiceUpper + 1) `div` 2, (twiceLower + 1) `div` 2)
+
+sampleColorRange :: Int -> [Color] -> [Color]
+sampleColorRange n rangePoints = case (n, rangePoints) of
+                (n', _)  | n' <= 0  -> []
+                (_, [])             -> replicate n black
+                (_, [color])        -> replicate n color
+                (1, (r:rps))        -> let [a,b,c] = replicate 3 rangePoints in b
+                (_, _)              -> [rangePoints!!0]
+                                         ++ [ interColor (rangePoints!!rpIndex) (rangePoints!!(rpIndex+1)) fraction
+                                              | i <- [1..(n-2)],
+                                                let (rpIndex, fraction) = properFraction (fromIntegral (length rangePoints) * fromIntegral i / (fromIntegral (n-1) :: Float) )]
+                                         ++ [rangePoints!!(n-1)]
+
+tolerantParamBuilder :: OkbParam -> [Float] -> [Int] -> [Color] -> OkbParam
+tolerantParamBuilder defaultP canvasPs gridPs colors =
+  let (stdCanvasPs, stdGridPs, stdColors) = rawParamDeconstructor defaultP
+      -- update[what] step <remaining new parameters> <remaining std parameters> -> <resulting parameters>
+      -- [what] = Canvas
+      -- steps: (0 canvas size) (1 (canvas pixel / unit of image)) (2 position of image origin on canvas)
+      updateCanvas step (n:m:ns) (d:e:ds) = (if step < 3 then  n:m:(updateCanvas (step+1) ns ds) else [])
+      updateCanvas 0    [n]      (d:e:ds) = n:(updateHeightKeepRatio d e n):ds -- updating canvas size
+      updateCanvas 1    [n]      (d:e:ds) = n:(updateHeightKeepRatio d e n):ds -- updating (canvas pixel / unit of image)
+      updateCanvas 2    [n]      (d:e:ds) = [n,e] -- updating position of image origin on canvas
+      updateCanvas step [] allds@(d:e:ds) = allds
+      updateCanvas _    _              ds = ds -- should never occur
+      -- [what] = Grid
+      -- steps: (0 octave range) (1 fifths range) (2 position of image origin on canvas)
+      updateGrid :: Int -> [Int] -> [Int] -> [Int]
+      updateGrid step newP defaultP = case (step, newP, defaultP) of
+        (0,    nt@[n,m,l], ds@[dl,dh,el,eh,fl,fh])   | n >= 0 && m >= 0 && l >= 0  -> foldr (\(a,b) acc -> a:b:acc) [] ({-new ranges as pairs-} zipWith3 updateRange nt [dl,el,fl] [dh,eh,fh])
+        (step, (n:m:ns),   allds@(d:e:ds))           | n <= m                      -> n:m:(updateGrid (step+1) ns ds)
+        (step, (n:ns),     allds@(d:e:ds))           | n >= 0                      -> let (n',m') = updateRange d e n in n':m':(updateGrid (step+1) ns ds)
+                                                     | otherwise                   -> n:(n+e-d):(updateGrid (step+1) ns ds)
+        (step, [],         allds)                                                  -> allds
+        (step,  _,         _)                                                      -> []
+      updatedGridParameters :: [Int]
+      updatedGridParameters = (updateGrid   0   gridPs   stdGridPs)
+      updatedColors = let
+        stdLevelMin = stdGridPs!!4
+        stdLevelMax = stdGridPs!!5
+        stdDefaultColor = stdColors!!(stdLevelMax-stdLevelMin+1)
+        newLevelMin = updatedGridParameters!!4
+        newLevelMax = updatedGridParameters!!5
+        newLevelCard = newLevelMax - newLevelMin + 1
+        in
+          case colors of
+            []                                     -> [ c | level <- [newLevelMin..newLevelMax], let c = if elem level [stdLevelMin..stdLevelMax]
+                                                                                                         then stdColors!!(level-stdLevelMin)
+                                                                                                         else stdDefaultColor] ++ [stdDefaultColor]
+            c:cs  | length colors >  newLevelCard  -> take (newLevelCard + 1) colors
+                  | length (c:cs) == newLevelCard  -> take newLevelCard colors ++ [stdDefaultColor]
+                  | otherwise                      -> sampleColorRange newLevelCard colors ++ [stdDefaultColor]
+  in
+    rawParamBuilder (updateCanvas 0 canvasPs stdCanvasPs)
+                    updatedGridParameters
+                    updatedColors
+
 
 okbgen :: OkbParam -> Element
 okbgen (OkbParam (sizeX, sizeY) (orX, orY) (scaleX, scaleY) (octMin, octMax) (fifthsMin, fifthsMax) (thirdsMin, thirdsMax) colorMap) = svg picture where
--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,8 @@
 module Main where
 
 import Okbgen
-fotoColors :: Int -> String
-fotoColors (-2) = "#0f0e14"
-fotoColors (-1) = "#9db2c5"
-fotoColors    0 = "#c2c0b9"
-fotoColors    1 = "#c48569"
-fotoColors    2 = "#3f2a1e"
-fotoColors    _ = "#ffffff"
 
-height, width, originx, originy, scalex, scaley :: Float
-height = 1080
-width = 1920
-originx = 1000
-originy = 540
-scalex = 400
-scaley = -400
 
-octsMin, octsMax, fifthsMin, fifthsMax, thirdsMin, thirdsMax :: Int
-octsMin = -8
-octsMax = 8
-fifthsMin = -10
-fifthsMax = 10
-thirdsMin = -2
-thirdsMax = 2
-
-stdParams :: OkbParam
-stdParams =  OkbParam { opCanvasSize = (width, height),
-                        opOriginOnCanvas = (originx, originy),
-                        opCanvasPxPerUnit = (scalex, scaley),
-                        opGridOctavesRange = (octsMin, octsMax),
-                        opGridFifthsRange = (fifthsMin, fifthsMax),
-                        opGridThirdsRange = (thirdsMin, thirdsMax),
-                        opKeyColors = HTMLColor . fotoColors }
 main :: IO ()
 main = print $ okbgen stdParams
 main :: IO ()
