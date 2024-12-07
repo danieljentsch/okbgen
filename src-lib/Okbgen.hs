07e3c3f4f3947647b984b9efc17096af7fb78bd8
danieljentsch <dev@danieljentsch.com>
2024-12-07 02:42:44 +0100

    Add clone function of okbgen: geotest

--- src-lib/Okbgen.hs
+++ src-lib/Okbgen.hs
@@ -1,4 +1,4 @@
-module Okbgen (Color(..), mkColorHTML, getColorCode, tolerantParamBuilder, stdParams, rawParamDeconstructor, orBlack, OkbParam(..), okbgen, Element) where
+module Okbgen (Color(..), mkColorHTML, getColorCode, tolerantParamBuilder, stdParams, rawParamDeconstructor, orBlack, OkbParam(..), okbgen, geotest, Element) where
 
 import Okbgen.Inner
 import Graphics.Svg (Element)
