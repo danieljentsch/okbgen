32e197f42cdc3e5eeb0818f3bd206602c68bbdfb
danieljentsch <dev@danieljentsch.com>
2025-02-21 19:51:23 +0100

    Fix init script to produce execution script specifying executable

--- init_with_cabal.sh
+++ init_with_cabal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 cabal update &&
     cabal build &&
-    printf "#!/usr/bin/sh\ncabal run -v0\n" > run_okbgen.sh &&
+    printf "#!/usr/bin/sh\ncabal run -v0 exe:okbgen\n" > run_okbgen.sh &&
     chmod u+x run_okbgen.sh
